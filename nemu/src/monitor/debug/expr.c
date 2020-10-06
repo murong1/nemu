aae545dfa70a6b1e5b5255f74adb97fdf68adc0c
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-06 15:12:11 +0000

    >  compile NEMU 3019244397 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:12:11 up  4:22,  1 user,  load average: 0.25, 0.18, 0.10 f15a12ae9c2cf60c82b45e65172644c5691297cf

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -244,7 +244,7 @@ int eval(int p,int q)
 	}
 	else if(check_parentheses(p,q) != 1)
 	{
-		printf("shizhe?\n");
+		//printf("shizhe?\n");
 		int op = find_dominant_op(p,q);
 		//printf("%d %d",p,q);
 		printf("op is %d\n",tokens[op].type);
@@ -263,7 +263,7 @@ int eval(int p,int q)
 		//return 0;
 		int val1 = eval(p,op-1);
 		int val2 = eval(op+1,q);
-		printf("fenkai %d,%d\n",val1,val2);
+		//printf("fenkai %d,%d\n",val1,val2);
 		switch(tokens[op].type)
 		{
 			case '+': return val1+val2;
@@ -278,11 +278,11 @@ int eval(int p,int q)
 	}
 	else 
 	{
-		printf("tuoke\n");
+		//printf("tuoke\n");
 		return eval(p+1,q-1);
 		
 	}
-	printf("zhecuole?\n");
+	//printf("zhecuole?\n");
 	Assert(1,"?");
 	return 0;
 
