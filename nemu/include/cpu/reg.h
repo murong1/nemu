74cc3d3903824e73f962f011ac2a95dc8f552bdc
tracer-ics2016 <tracer@njuics.org>
2020-11-03 15:57:23 +0000

    >  compile NEMU 3019244397 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:57:23 up  3:43,  1 user,  load average: 1.32, 1.50, 1.54 f1374d11aa881152e3165f6be88e17bb47f535c

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -2,7 +2,7 @@
 #define __REG_H__
 
 #include "common.h"
-
+#include "../../../lib-common/x86-inc/cpu.h"
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
@@ -14,6 +14,26 @@ enum { R_ES, R_CS, R_SS, R_DS, R_FS, R_GS };
  * cpu.gpr[1]._8[1], we will get the 'ch' register. Hint: Use `union'.
  * For more details about the register encoding scheme, see i386 manual.
  */
+
+struct SREG{
+		uint16_t selector;
+		union {
+			struct {
+				uint32_t seg_base1 :16;
+				uint32_t seg_base2 :8;
+				uint32_t seg_base3 :8;
+			};
+			uint32_t seg_base;
+		};
+		union {
+			struct {
+				uint32_t seg_limit1 :16;
+				uint32_t seg_limit2 :4;
+				uint32_t seg_limit3 :12;
+			};
+			uint32_t seg_limit;
+		};
+};
 
 typedef struct {
      union{
@@ -64,7 +84,16 @@ static inline int check_reg_index(int index) {
 	assert(index >= 0 && index < 8);
 	return index;
 }
-
+	CR0 cr0;
+	CR3 cr3;
+/*
+union {
+        	struct SREG sr[6];
+        	struct 
+        	{
+            struct SREG es, cs, ss, ds, fs, gs;
+        	};
+};*/
 
 #define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
 #define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -3,8 +3,8 @@
 #define instr mov
 
 static void do_execute() {
-	OPERAND_W(op_dest, op_src->val);
-	print_asm_template2();
+OPERAND_W(op_dest, op_src->val);
+print_asm_template2();
 }
 
 make_instr_helper(i2r)
@@ -13,19 +13,19 @@ make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 
 make_helper(concat(mov_a2moffs_, SUFFIX)) {
-	swaddr_t addr = instr_fetch(eip + 1, 4);
-	MEM_W(addr, REG(R_EAX));
+swaddr_t addr = instr_fetch(eip + 1, 4);
+MEM_W(addr, REG(R_EAX));
 
-	print_asm("mov" str(SUFFIX) " %%%s,0x%x", REG_NAME(R_EAX), addr);
-	return 5;
+print_asm("mov" str(SUFFIX) " %%%s,0x%x", REG_NAME(R_EAX), addr);
+return 5;
 }
 
 make_helper(concat(mov_moffs2a_, SUFFIX)) {
-	swaddr_t addr = instr_fetch(eip + 1, 4);
-	REG(R_EAX) = MEM_R(addr);
+swaddr_t addr = instr_fetch(eip + 1, 4);
+REG(R_EAX) = MEM_R(addr);
 
-	print_asm("mov" str(SUFFIX) " 0x%x,%%%s", addr, REG_NAME(R_EAX));
-	return 5;
+print_asm("mov" str(SUFFIX) " 0x%x,%%%s", addr, REG_NAME(R_EAX));
+return 5;
 }
 
 #include "cpu/exec/template-end.h"
