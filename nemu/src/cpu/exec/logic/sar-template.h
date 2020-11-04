207762ff06efebad69d0a61f8f6b6ccadc96b019
tracer-ics2016 <tracer@njuics.org>
2020-11-04 04:14:14 +0000

    >  compile NEMU 3019244397 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:14:14 up 16:00,  1 user,  load average: 2.39, 1.65, 1.41 dcba594961e64152fda9786fd8f48ce4807caf1

--- nemu/src/cpu/exec/logic/sar-template.h
+++ nemu/src/cpu/exec/logic/sar-template.h
@@ -5,12 +5,20 @@
 static void do_execute () {
 	DATA_TYPE src = op_src->val;
 	DATA_TYPE_S dest = op_dest->val;
-
+	DATA_TYPE_S result = op_dest->val;
 	uint8_t count = src & 0x1f;
 	dest >>= count;
-	OPERAND_W(op_dest, dest);
-	
-	concat(updateCPU_, SUFFIX) (dest);
+	result = dest;
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF=0;
+	cpu.OF=0;
+	cpu.SF=result >> len;
+    	cpu.ZF=!result;
+    	OPERAND_W(op_dest, dest);
+	result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.PF=!(result & 1);
 	/* There is no need to update EFLAGS, since no other instructions 
 	 * in PA will test the flags updated by this instruction.
 	 */
