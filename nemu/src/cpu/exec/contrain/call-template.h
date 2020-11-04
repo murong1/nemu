4ea9bd5df76a8fc7b6cb7bb85620b7d8fbfdba91
tracer-ics2016 <tracer@njuics.org>
2020-11-04 10:57:12 +0000

    >  compile NEMU 3019244397 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:57:12 up  2:56,  1 user,  load average: 1.29, 1.07, 0.91 e448d6d259d6be97d1f941f506ba35e105d4430

--- nemu/src/cpu/exec/contrain/call-template.h
+++ nemu/src/cpu/exec/contrain/call-template.h
@@ -2,17 +2,27 @@
 
 #define instr call
 
-
-make_helper(concat(call_i_, SUFFIX)) {
-	int len = concat(decode_i_,SUFFIX)(cpu.eip + 1);
-
-    reg_l(R_ESP) -= DATA_BYTE;
-    swaddr_write(reg_l(R_ESP),4,cpu.eip + len + 1);
-    DATA_TYPE_S imm = op_src -> val;
-    print_asm("call\t%x",cpu.eip + 1 +len + imm);
-    cpu.eip+=imm;
-    return len + 1;
+make_helper (concat(call_i_, SUFFIX))
+{
+	int len = concat(decode_i_, SUFFIX) (eip + 1);
+	reg_l (R_ESP) -= DATA_BYTE;
+	swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len);
+	DATA_TYPE_S displacement = op_src->val;
+	print_asm("call %x",cpu.eip + 1 + len + displacement);
+	cpu.eip +=displacement;
+	return len + 1;
+}
+make_helper (concat(call_rm_, SUFFIX))
+{
+	int len = concat(decode_rm_, SUFFIX) (eip + 1);
+	reg_l (R_ESP) -= DATA_BYTE;
+	swaddr_write (reg_l (R_ESP) , 4 , cpu.eip + len);
+	DATA_TYPE_S displacement = op_src->val;
+	print_asm("call %x",displacement);
+	cpu.eip = displacement - len - 1;
+	return len + 1;
 }
 
 
-#include  "cpu/exec/template-end.h"
+
+#include "cpu/exec/template-end.h"
