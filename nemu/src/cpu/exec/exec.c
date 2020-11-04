278358c6e52888bf2e25b1b387bf5e9fc86c3ca5
tracer-ics2016 <tracer@njuics.org>
2020-11-04 04:44:42 +0000

    >  compile NEMU 3019244397 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:44:42 up 16:31,  1 user,  load average: 1.22, 1.33, 1.46 8a60ac9c3bc259f94ea491675ce377060803b81

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -108,7 +108,7 @@ helper_fun opcode_table [256] = {
 /* 0x2c */	inv, inv, inv, inv,
 /* 0x30 */	inv, inv, inv, inv,
 /* 0x34 */	inv, inv, inv, inv,
-/* 0x38 */	inv, inv, inv, inv,
+/* 0x38 */	cmp_r2rm_b, cmp_r2rm_v, cmp_r2rm_b, cmp_rm2r_v,
 /* 0x3c */	inv, inv, inv, inv,
 /* 0x40 */	inv, inv, inv, inv,
 /* 0x44 */	inv, inv, inv, inv,
