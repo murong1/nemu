201b58e300ad139495dc3da3eaf5cbd887a8dc12
tracer-ics2016 <tracer@njuics.org>
2020-11-04 12:54:53 +0000

    >  run 3019244397 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:54:53 up  4:53,  1 user,  load average: 0.85, 1.00, 1.03 8a72f149572cda7d5048600dfd2d74c9acc05485

--- testcase/src/ceshi1.c
+++ testcase/src/ceshi1.c
@@ -3,12 +3,14 @@
 int main()
 {
     int  i;
-   for( i=0;i<5;i++)
+   for( i=0;i<2;i++)
    {
 
    }
-   nemu_assert(i == 3);
-  nemu_assert(i == 4);
+   nemu_assert(i == 1);
+   nemu_assert(i == 2);
+    nemu_assert(i == 3);
+    nemu_assert(i == 4);
 
     return 0;
 }
